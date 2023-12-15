d4fce92c4c784f03e287fd02d7cfa1a557470421
Dmitriy <skokdmitriy@gmail.com>
2023-12-16 00:45:35 +0700

    init

---
+++ HotelApp/HotelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelAppApp.swift
+//  HotelApp
+//
+//  Created by Дмитрий Скок on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
